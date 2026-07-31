75787afc31139a28095b4f2c5b740905da78a020
lukaijie <lukaijie@shopex.cn>
2026-07-31 11:59:46 +0800

    [TBID:XXX] Initial commit

---
+++ src/components/ToggleRow.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: boolean
+  title: string
+  text: string
+}>()
+
+defineEmits<{
+  'update:modelValue': [value: boolean]
+}>()
+</script>
+
+<template>
+  <div class="toggle-row">
+    <div>
+      <b>{{ title }}</b>
+      <p>{{ text }}</p>
+    </div>
+    <button
+      type="button"
+      :class="{ on: modelValue }"
+      :aria-label="title"
+      :aria-pressed="modelValue"
+      @click="$emit('update:modelValue', !modelValue)"
+    >
+      <i></i>
+    </button>
+  </div>
+</template>
